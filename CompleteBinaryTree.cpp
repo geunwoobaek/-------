df08e8853971ccbca5a9ffbe3d4fd8c0e7ad4ca8
geunwoobaek <39114544+geunwoobaek@users.noreply.github.com>
2020-11-26 21:54:28 +0900

    _

---
+++ CompleteBinaryTree.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include<queue>
+using namespace std;
+
+class node
+{
+    int value;
+};
+class Tree
+{
+private:
+    priority_queue<int> que;
+    node cur;
+    node *RightNode;
+    node *LeftNode;
+public:
+    void insert(int value){
+
+    }
+}
--- SegmentTree/tree.cpp
+++ SegmentTree/tree.cpp
@@ -1,6 +1,13 @@
 #include<iostream>
 using namespace std;
+struct node{
+int value,sum;
+};
 class SegmentTree{
+private:
+node value;
+
+public:
 
 };
 int main()
