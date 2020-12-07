890ac6db84066d8b2c2560a3136d6ab926099547
geunwoobaek <39114544+geunwoobaek@users.noreply.github.com>
2020-12-07 20:42:15 +0900

    이중트리구현
    _

---
+++ binarySearchTree/normal.cpp
@@ -0,0 +1,105 @@
+#include <bits/stdc++.h>
+using namespace std;
+struct node
+{
+    node *parent = NULL;
+    node *left = NULL;
+    node *right = NULL;
+    int data = 0;
+    node(int value)
+    {
+        data = value;
+    }
+    void push(node *prev, int value)
+    {
+        if (data < value)
+        {
+            if (right == NULL)
+                right = makenode(value);
+            else
+                right->push(this, value);
+        }
+        else if(data>value)
+        {
+            if (left == NULL)
+                left = makenode(value);
+            else
+                left->push(this, value);
+        }
+        else cout<<"이미 존재합니다."<<endl;
+    }
+    void delete_(int value)
+    {
+        if (data < value)
+        {   
+            if( right == NULL) cout << "존재하지않음" << endl; //비어있지않을때
+            else right->delete_(value);
+        }
+        else if (data < value)
+        {   
+            if( left == NULL) cout << "존재하지않음" << endl; //비어있지않을때
+            else left->delete_(value);
+        }
+        else
+        {
+            node *removenow = this;
+            left->parent = parent;
+            right->parent = parent;
+            parent->left = this->left;
+            parent->right = this->right;
+            delete removenow;
+        }
+    }
+    void preorder()
+    {
+        cout << data << " ";
+        if (left != NULL)
+            left->preorder();
+        if (right != NULL)
+            right->preorder();
+    }
+    node *makenode(int value)
+    {
+        node *new_node = new node(value);
+        new_node->parent = this;
+        return new_node;
+    }
+};
+struct BinarySearchTree
+{
+    node *root = NULL;
+    void push(int value);
+    void delete_(int value);
+    void clear();
+    void preorder();
+};
+void BinarySearchTree::push(int value)
+{
+    if (root == NULL)
+        root = new node(value);
+    else
+        root->push(root, value);
+}
+void BinarySearchTree::delete_(int value)
+{
+    if (root == NULL)
+        cout << "empty";
+    else
+        root->delete_(value);
+}
+void BinarySearchTree::preorder()
+{
+    if (root != NULL)
+        root->preorder();
+}
+int main()
+{
+    BinarySearchTree tree;
+    for (int i = 0; i < 15; i++)
+    {
+        tree.push(rand() % 15 + 1);
+    }
+    tree.preorder();
+    cout << "";
+    return 0;
+}
